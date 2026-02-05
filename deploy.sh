762a6f19f3a344c002db051615242ef686a676f9
Gerlandio Bernardo <gerlandios51@gmail.com>
2026-02-05 11:37:40 -0300

    feat: adiciona script deploy.sh para execução do Terraform

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Inicializando o Terraform..."
+terraform init
+
+if [ $? -ne 0 ]; then
+  echo "Erro no comando terraform init"
+  exit 1
+fi
+
+echo "Validando os arquivos Terraform..."
+terraform validate
+
+if [ $? -ne 0 ]; then
+  echo "Erro na validação dos arquivos Terraform"
+  exit 1
+fi
+
+echo "Gerando o plano de execução..."
+terraform plan -out=tfplan
+
+if [ $? -ne 0 ]; then
+  echo "Erro no comando terraform plan"
+  exit 1
+fi
+
+echo "Aplicando a infraestrutura (ec2 e bucket s3)..."
+terraform apply tfplan
+
+if [ $? -ne 0 ]; then
+  echo "Erro no comando terraform apply"
+  exit 1
+fi
+
+echo "Infraestrutura (ec2 e bukert s3) criada com sucesso!"
